5f406bab1d4f7314af86ea9155a136c32f861952
nora <51029577+norayao@users.noreply.github.com>
2019-07-27 16:23:55 -0300

    Update Zinc.sql

--- DB/Zinc.sql
+++ DB/Zinc.sql
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS `Part` (
   PRIMARY KEY (`partID`),
   UNIQUE INDEX `partID_UNIQUE` (`partID` ASC));
 
-
+insert into Part values('ISO-30001','zinc','oweuru');
 -- -----------------------------------------------------
 -- Table `Warehouse`
 -- -----------------------------------------------------
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS `Warehouse` (
   UNIQUE INDEX `warehouseID_UNIQUE` (`warehouseID` ASC),
   PRIMARY KEY (`warehouseID`));
 
-
+insert into Warehouse values(40001,'z','halifax');
 -- -----------------------------------------------------
 -- Table `Stock`
 -- -----------------------------------------------------
@@ -44,3 +44,4 @@ CREATE TABLE IF NOT EXISTS `Stock` (
     REFERENCES `Warehouse` (`warehouseID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
+insert into Stock values(30,5,'ISO-30001',40001);
