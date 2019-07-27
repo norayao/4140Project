3c2b99e727cad6c27416a45df89f567095eeede4
nora <51029577+norayao@users.noreply.github.com>
2019-07-27 16:21:11 -0300

    Update newFront.sql

--- DB/newFront.sql
+++ DB/newFront.sql
@@ -12,6 +12,8 @@ CREATE TABLE IF NOT EXISTS Customer (
   PRIMARY KEY (customerID),
   UNIQUE INDEX customerID_UNIQUE (customerID ASC));
 
+insert into Customer values(10001,'Yihong Yao','989-8888','2393 robie st');
+
 
 -- -----------------------------------------------------
 -- Table `Invoice`
@@ -32,7 +34,7 @@ CREATE TABLE IF NOT EXISTS Invoice (
     REFERENCES `Customer` (`customerID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
-
+insert into Invoice values(20001,curdate(),10001,'ISO-30001',30,true);
 
 
 
