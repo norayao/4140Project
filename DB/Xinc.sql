bfc0b5edd6bd70b2ea0aaf0b5ae3a32000cf04e7
nora <51029577+norayao@users.noreply.github.com>
2019-07-28 16:28:44 -0300

    Update Xinc.sql

--- DB/Xinc.sql
+++ DB/Xinc.sql
@@ -8,11 +8,18 @@ USE Xinc;
 CREATE TABLE IF NOT EXISTS `Part` (
   `partID` VARCHAR(45) NOT NULL,
   `partName` VARCHAR(45) NOT NULL,
+  `partPrice` VARCHAR(10) NOT NULL,
   `partDescription` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`partID`));
 
-insert into Part values('30001','xinc','skfhsh');
-
+insert into Part values('30001','unknown','10.5','skfhsh');
+insert into Part values('30002','analog machine','30.0','blue');
+insert into Part values('30003','charging machine','4.5','white');
+insert into Part values('30004','calculator','15.0','red');
+insert into Part values('30005','ruler','2.0','black');
+insert into Part values('30006','gear','1.0','green');
+insert into Part values('30007','araloop','14.0','grey');
+insert into Part values('30008','bottle','30','yellow');
 -- -----------------------------------------------------
 -- Table `Warehouse`
 -- -----------------------------------------------------
@@ -46,3 +53,11 @@ CREATE TABLE IF NOT EXISTS `Stock` (
     ON UPDATE NO ACTION);
     
     insert into Stock values(30,5,'30001',40001);
+    insert into Stock values(20,3,'30002',40001);
+    insert into Stock values(45,3,'30003',40001);
+    insert into Stock values(25,3,'30004',40001);
+    insert into Stock values(20,3,'30005',40001); 
+    insert into Stock values(10,3,'30006',40001);
+    insert into Stock values(12,3,'30007',40001);
+    insert into Stock values(20,3,'30008',40001); 
+
